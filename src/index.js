c43983f932ba01a85f4052404bb722301a5de4b8
Matthew Henderson <info@matthewahenderson.com>
2022-08-14 15:06:54 -0400

    Changed main app component to class-based component; added lifecycle method for getting geolocation; added functions for getting season and season config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    return <div>Hi there!</div>;
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+     
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>;
+        }
+        
+        return <div>Loading!</div>;
+    }
+}
 
 ReactDOM.render(
     <App />,
